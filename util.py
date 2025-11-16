2d9105cac8ada9704d5f582567cfff796005096e
sjapanjots <sjapanjots@gmail.com>
2025-11-16 11:33:13 +0530

    fixed parametric and syntax errors

--- util.py
+++ util.py
@@ -7,12 +7,9 @@ def get_angle(a ,b ,c):
     angle = np.abs(np.degrees(radians))
     return angle
 
-
-
 def get_distance(landmark_list):
     if len(landmark_list) < 2:
         return
-    
     
     (x1, y1) , (x2, y2) = landmark_list[0], landmark_list[1]
     L= np.hypot(x2 - x1, y2 - y1)
